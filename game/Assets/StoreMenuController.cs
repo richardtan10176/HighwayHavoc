e575cef82a7597046097927793db6c469b18b5a8
Richard Tan <richardtan787@gmail.com>
2023-06-17 22:27:55 -0400

    made it portrait and added some triggers for events

--- game/Assets/StoreMenuController.cs
+++ game/Assets/StoreMenuController.cs
@@ -9,9 +9,13 @@ public class StoreMenuController : MonoBehaviour
     public Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
     static public int userGearsAmount;
     static public int use;
+    public static GameObject CarStore, gemStore;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        GameObject CarStore = new GameObject();
+        GameObject gemStore = new GameObject();
+
         button1.onClick.AddListener(TaskOnClick);
         button2.onClick.AddListener(TaskOnClick);
         button3.onClick.AddListener(TaskOnClick);
@@ -21,12 +25,31 @@ public class StoreMenuController : MonoBehaviour
         button7.onClick.AddListener(TaskOnClick);
         button8.onClick.AddListener(TaskOnClick);
         button9.onClick.AddListener(TaskOnClick);
+
+
     }
-    void TaskOnClick()
-    {    }
+    void TaskOnClick() { }
     // Update is called once per frame
     void Update()
     {
         
     }
+    static void enableCarStore()
+    {
+        CarStore.SetActive(true);
+    }
+    static void disableCarStore()
+    {
+        CarStore.SetActive(false);
+    }
+    static void enableGemStore()
+    {
+        CarStore.SetActive(true);
+    }
+    static void disableGemStore()
+    {
+        CarStore.SetActive(false);
+    }
+   
+
 }
