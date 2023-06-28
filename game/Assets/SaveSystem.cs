d270c2c91362be8a261522375111d12f1ebd30ef
Richard Tan <richardtan787@gmail.com>
2023-06-28 14:52:23 -0400

    changed savesystem

--- game/Assets/SaveSystem.cs
+++ game/Assets/SaveSystem.cs
@@ -6,7 +6,7 @@ public static class SaveSystem
     public static void SavePlayer(Player player)
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.aet";
+        string path = Application.persistentDataPath + "/player.ae";
         FileStream stream = new FileStream(path, FileMode.Create);
         PlayerData data = new PlayerData(player);
 
@@ -16,7 +16,7 @@ public static class SaveSystem
 
     public static PlayerData LoadPlayer()
     {
-        string path = Application.persistentDataPath + "/player.aet";
+        string path = Application.persistentDataPath + "/player.ae";
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
