a48fefe779c4b627315abe81122d5ef654d1c1ba
Richard Tan <richardtan787@gmail.com>
2023-07-19 21:50:43 -0400

    fixed IAP (finally)

---
+++ Highway Havoc/Assets/Scripts/Level/CollisionDetection.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+	public bool hasRespawned = false;
+	Playermovement playermovement;
+	GameObject DeathScreen;
+	GameObject respawnScreen;
+
+    void Start()
+    {
+		playermovement = this.GetComponent<Playermovement>();
+		DeathScreen = GameObject.Find("DeathScreen").transform.GetChild(0).gameObject;
+		respawnScreen = GameObject.Find("RespawnScreen").transform.GetChild(0).gameObject;
+	}
+    void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.transform.tag == "Obstacle")
+		{
+			PlayerPrefs.SetInt("coinScore", PlayerPrefs.GetInt("coinScore") + Playermovement.NumOfCoins);
+			PlayerPrefs.SetInt("score", Score.pScore);
+			int randNum = Random.Range(1, 5);
+			playermovement.playerMove = false;
+			
+			playermovement.fire.transform.position = SpawnPlayerCar.playerCar.transform.position + new Vector3(0, 0, 1);
+			playermovement.explosion.transform.position = SpawnPlayerCar.playerCar.transform.position + new Vector3(0,0,5);
+			playermovement.forwardSpeed = 0;
+			playermovement.explosion.SetActive(true);
+			playermovement.fire.SetActive(true);
+			DeathScreen.SetActive(true);
+
+			if(randNum == 3 && !hasRespawned)
+            {
+				respawnScreen.SetActive(true);
+            }
+
+			
+		}
+	}
+}
+
---
+++ Highway Havoc/Assets/Scripts/MainMenu/UIController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject DeathScreen;
+    public GameObject RespawnScreen;
+
+    public Button retryButton;
+    public Button returnToMenuButton;
+
+    public GameObject InsFunds;
+    public Button closePopupButton;
+    public Button respawnButton;
+    void Start()
+    {
+        respawnButton.onClick.AddListener(respawn);
+        retryButton.onClick.AddListener(retry);
+        returnToMenuButton.onClick.AddListener(returnToMenu);
+        closePopupButton.onClick.AddListener(closePopup);
+    }
+
+    void retry()
+    {
+        //PlayerPrefs.SetInt("coinScore", PlayerPrefs.GetInt("coinScore") + Playermovement.NumOfCoins);
+        if(PlayerPrefs.GetInt("score") > PlayerPrefs.GetInt("highScore"))
+        {
+            PlayerPrefs.SetInt("highScore", PlayerPrefs.GetInt("score"));
+        }
+        SceneManager.LoadScene(1);
+    }
+    
+    void returnToMenu()
+    {
+        DeathScreen.SetActive(false);
+        RespawnScreen.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+    void respawn()
+    {
+        if(loadPlayerData.gemsAmount >= 20)
+        {
+            DeathScreen.SetActive(false);
+            RespawnScreen.SetActive(false);
+            SpawnPlayerCar.playerCar.GetComponent<Rigidbody>().isKinematic = true;
+            SpawnPlayerCar.playerCar.GetComponent<Playermovement>().playerMove = true;
+            SpawnPlayerCar.playerCar.GetComponent<Playermovement>().explosion.SetActive(false);
+            SpawnPlayerCar.playerCar.GetComponent<Playermovement>().fire.SetActive(false);
+            SpawnPlayerCar.playerCar.GetComponent<Playermovement>().forwardSpeed = 8;
+            SpawnPlayerCar.playerCar.GetComponent<CollisionDetection>().hasRespawned = true;
+            StartCoroutine(DisableCrashing(5.0f));
+            InvokeRepeating("flash", 0, 0.25f);
+        }
+        else
+        {
+            InsFunds.SetActive(true);
+        }
+        
+
+    }
+    void closePopup()
+    {
+        Debug.Log("bruh");
+        InsFunds.SetActive(false);
+    }
+    void flash()
+    {
+        SpawnPlayerCar.playerCar.transform.GetChild(0).GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = !SpawnPlayerCar.playerCar.transform.GetChild(0).GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled;
+    }
+    IEnumerator DisableCrashing(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SpawnPlayerCar.playerCar.GetComponent<Rigidbody>().isKinematic = false;
+        CancelInvoke("flash");
+        SpawnPlayerCar.playerCar.transform.GetChild(0).GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+
+    }
+}
