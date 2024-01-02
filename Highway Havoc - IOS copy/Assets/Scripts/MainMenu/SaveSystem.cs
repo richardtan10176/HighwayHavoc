212de29c12cba2b66e7ca75fb0c8474f677d4239
Richard Tan <richardtan787@gmail.com>
2024-01-01 22:07:00 -0500

    safety commit

--- Highway Havoc - IOS copy/Assets/Scripts/MainMenu/SaveSystem.cs
+++ Highway Havoc - IOS copy/Assets/Scripts/MainMenu/SaveSystem.cs
@@ -6,7 +6,7 @@ public static class SaveSystem
     public static void SavePlayer(Player player)
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/playerdata.ballsssssssssss";
+        string path = Application.persistentDataPath + "/playerdata.hw";
         FileStream stream = new FileStream(path, FileMode.Create);
         PlayerData data = new PlayerData(player);
 
@@ -16,7 +16,7 @@ public static class SaveSystem
 
     public static PlayerData LoadPlayer()
     {
-        string path = Application.persistentDataPath + "/playerdata.ballsssssssssss";
+        string path = Application.persistentDataPath + "/playerdata.hw";
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
